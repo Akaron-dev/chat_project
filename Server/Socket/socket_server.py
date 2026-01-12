12c8d807553d8c3581fbc7cf93cae093e7013c67
Akaron-dev <timfrauenfelder4@gmail.com>
2026-01-12 09:49:46 +0100

    Threading completion
    Working Multi-threading for server requests

--- Server/Socket/socket_server.py
+++ Server/Socket/socket_server.py
@@ -1,31 +1,31 @@
 import socket
-import time
 import threading
+from _thread import start_new_thread
+lock = threading.Lock()
 
-HOST = '127.0.0.1'
-PORT = 3747
 
-def process_request(communication_socket, address, communication_socket_backup):
-    threading.Thread(target=preprocess_request, args=[communication_socket_backup]).run()
-    print(f"Connected to {address}")
-    message = communication_socket.recv(1024).decode('utf-8')
-    print(f"Messgae from client is: {message}")
-    communication_socket.send(f"Got your message! PS: You gay".encode('utf-8'))
-    communication_socket.close()
-    print(f"Connection with {address} ended!")
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-
-server.listen()
-communication_socket = ()
-communication_socket_backup = communication_socket
-def preprocess_request(communication_socket_backup):
+def process_request(communication_socket):
     while True:
-        communication_socket_backup = communication_socket
-        communication_socket, address = server.accept()
-        if communication_socket != communication_socket_backup:
-            name = threading.active_count()
-            threading.Thread(target=process_request, args=[communication_socket, address, communication_socket_backup]).run()
+        data = communication_socket.recv(2048)
+        if not data:
+            print('bye')
+            lock.release()
+            break
+        communication_socket.send(data[::-1])
+    communication_socket.close()
 
 
+def server_main():
+    HOST = ''
+    PORT = 3747
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(5)
+    while True:
+        communication_socket, address = s.accept()
+        lock.acquire()
+        print(f'Connected to {address[0]}:{address[1]}')
+        start_new_thread(process_request, (communication_socket,))
+
+
+server_main()
