25c6d19d839758e4432cbab40bdcfd43de58ccc7
Akaron-dev <timfrauenfelder4@gmail.com>
2026-08-05 01:03:09 +0000

    sqlite pt. 1

--- Code/Server/Socket/libserver.py
+++
@@ -1,6 +0,0 @@
-import re
-def data_to_msg(data):
-    data = data.decode('utf-8')
-    header = data.split(' ', 1)[0]
-    msg = data.split(' ', 1)[1]
-    return header, msg
--- Code/Server/account_handling.py
+++ Code/Server/account_handling.py
@@ -1,15 +1,4 @@
-import json
+import sqlite3
 
-
-Account_names = []
-new_user=()
-password_hash=()
-
-def create_account():
-    Account_names.append(new_user)
-    with open(f'/home/steinbock/chat_project/userdata/password_{new_user}.json', "a") as f:
-        f.write(json.dumps(password_hash))
-    open(f"/home/steinbock/chat_project/userdata/chatdata_{new_user}.json", "w")                  
-    print("Account was Created")                                                        #Send response to Client
 
 
